38de4760532a1d7906ff81e70e20653c0fe2317f
Mathi81 <thiagomatheusthebest@gmail.com>
2017-06-26 00:52:41 -0300

    segundo Commit para terminar

--- src/laboratorio3/MainTestes.java
+++ src/laboratorio3/MainTestes.java
@@ -1,25 +1,58 @@
 package laboratorio3;
 
+import java.util.Scanner;
+
 public class MainTestes {
 
 	public static void main(String[] args) {
-		Sistema a = new Sistema();
-		String  b = a.adicionarAlunos("222", "thiago", "cc");
-		String  c = a.adicionarAlunos("111", "thiago", "cc");
-		String  d = a.adicionarAlunos("222", "thiago", "cc");
-		String  e = a.adicionarAlunos("111", "thiago", "cc");
+
+		Scanner ler = new Scanner(System.in);
 		
-		String x = a.adicionarGrupos("lista");
-		String y = a.alocarAlunoGrupo("222", "lista");
-		String z = a.alocarAlunoGrupo("222", "lista");
-		String h = a.alocarAlunoGrupo("111", "lista");
-		String j = a.alocarAlunoGrupo("111", "lista");
+		Sistema sistema = new Sistema();
 		
-		String f = a.imprimirGrupo("lista");
+		boolean execucao = true;
 		
-		System.out.println(f);
+		while (execucao) {
+
+			System.out.println("(C)adastrar Aluno" + System.lineSeparator() + "(E)xibir Aluno" + System.lineSeparator()
+					+ "(N)ovo Grupo" + System.lineSeparator() + "(A)locar Aluno no Grupo e Imprimir Grupos"
+					+ System.lineSeparator() + "(R)egistrar Resposta de Aluno" + System.lineSeparator()
+					+ "(I)mprimir Alunos que Responderam" + System.lineSeparator() + "(O)ra, vamos fechar o programa!" + System.lineSeparator());
+					
+			System.out.print("Opções> ");
+			String opcoes = ler.nextLine();
+			
+			if (opcoes.equals("C")) {
+				System.out.print("Matricula: ");
+				String matricula = ler.next();
+				
+				
+				System.out.print("Nome: ");
+				String nome = ler.nextLine();
+				
+				System.out.println("Curso: ");
+				String curso = ler.nextLine();
+				
+				String saidaMatricula = sistema.adicionarAlunos(matricula, nome, curso);
+				
+				System.out.println(saidaMatricula);
+				
+				
+			} else if (opcoes.equals("E")) {
+				
+				System.out.print("Matricula");
+				String numeroMatricula = ler.nextLine();
+				System.out.println();
+				
+				String saidaConsultaAluno = sistema.consultarAluno(numeroMatricula);
+				System.out.println(saidaConsultaAluno);
+			
+			} else if (opcoes.equals("A")) {
+				
+			}
+		}
 		
-		System.out.println(a.imprimirAlunosQueRespondemQuestoes());
+		
 
 	}
 
